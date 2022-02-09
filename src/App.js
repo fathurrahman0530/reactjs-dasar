7474550230b4fc0ffd36aa410ed8e0c808788bc5
FATHUR RAHMAN ABDULLAH <63894938+fathurrahman0530@users.noreply.github.com>
2022-02-09 20:27:48 +0800

    update progress study react js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
+import Navbar from './Navbar.js';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Navbar />
       <h2>Selamat Datang di Pembelajaran ReactJS</h2>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+// class component
+// export default class Navbar extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h3>Navbar</h3>
+//       </div>
+//     )
+//   }
+// }
+
+// function component
+// export default function Navbar() {
+//   return (
+//     <div>
+//       <h3>Navbar</h3>
+//     </div>
+//   )
+// }
+
+// arrow component
+export const Navbar = () => {
+  return (
+    <div>
+      <h3>Navbar</h3>
+    </div>
+  )
+}
+
+export default Navbar
